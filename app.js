36d47c2d1102b049033da60568598adb153c2ea5
Chittipobng014 <tpl_army@hotmail.com>
2018-09-04 00:52:38 +0700

    commit

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.listen(3000);
 
 var clients = {};
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
   socket.on('add-user', function(data){
