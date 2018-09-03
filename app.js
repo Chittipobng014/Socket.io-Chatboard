efd2c2179bfc01a75f4592e6a85ed3fde27579d6
Chittipobng014 <tpl_army@hotmail.com>
2018-09-04 01:02:51 +0700

    port edit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var cors = require('cors');
 
-app.listen(3000);
+app.listen(process.env.PORT || 8080);
 
 var clients = {};
+
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
